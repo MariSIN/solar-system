01073fc3b91ec96a66696ee5ebca147c2dfd574e
Mariana Nascimento <mariana.sn.silva@gmail.com>
2022-09-21 15:09:51 -0300

    adiciona div

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import Header from './components';
+import Header from './components/Header';
+import SolarSystem from './components/solarSystem';
 
 class App extends React.Component {
   render() {
-    return (<Header />);
+    return (
+      <>
+        <Header />
+        <SolarSystem />
+      </>
+    );
   }
 }
 
