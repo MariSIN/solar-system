3425518f3a79b56afa6dd6f12ea09e5f6fba9bf2
Mariana Nascimento <mariana.sn.silva@gmail.com>
2022-09-22 14:45:04 -0300

    Adiciona estilização na lista e adiciona icons

--- src/components/MissionCard.js
+++ src/components/MissionCard.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { FaCalendar, FaLocationArrow, FaFlag } from 'react-icons/fa';
 
 class MissionCard extends Component {
   render() {
@@ -7,10 +8,25 @@ class MissionCard extends Component {
     return (
       <div className="mission-container">
         <div data-testid="mission-card">
-          <p data-testid="mission-name">{ name }</p>
-          <p data-testid="mission-year">{ year }</p>
-          <p data-testid="mission-country">{ country }</p>
-          <p data-testid="mission-destination">{ destination }</p>
+          <p data-testid="mission-name" className="name-mission">{ name }</p>
+          <div className="separador">{' '}</div>
+          <div className="dates">
+            <p data-testid="mission-year" className="year-mission">
+              <FaCalendar />
+              { year }
+            </p>
+            <p data-testid="mission-country" className="country-mission">
+              <FaLocationArrow />
+              { country }
+            </p>
+            <p
+              data-testid="mission-destination"
+              className="destiny"
+            >
+              <FaFlag />
+              {destination}
+            </p>
+          </div>
         </div>
       </div>
     );
